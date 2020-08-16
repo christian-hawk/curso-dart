eccb86a3b4127f153ce545737566b483a8b79e12
Christian H <eland.christian@gmail.com>
2020-08-16 18:33:24 +0000

    if-else

---
+++ estruturas_de_controle/if_else.dart
@@ -0,0 +1,15 @@
+import 'dart:math';
+
+main(List<String> args) {
+  var nota = Random().nextInt(11); // entre 0 e 10
+
+  if (nota >= 7) {
+    print("Aprovado");
+  } else if {
+    ...
+  }
+  if(true)
+    print('Aprovado');
+    print('Fim!');
+  // Fim!
+}
